473b7b2271ee307da73dd6c36b41b534ceb1a103
Victor Barbosa <victor.hugo.p.barbosa@hotmail.com>
2020-12-20 20:26:43 -0500

    first commit

---
+++ Core/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var startTime  = DateTime.Now;
+           Counter();
+           var endtime  = DateTime.Now;
+           var total = endtime - startTime;
+           Console.WriteLine("Start {0}",startTime);  
+           Console.WriteLine("End {0}",endtime);  
+           Console.WriteLine("Total {0}",total);   
+        }
+       static void Counter()
+       {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Console.WriteLine(i);   
+            }
+        }
+
+    }
+}
